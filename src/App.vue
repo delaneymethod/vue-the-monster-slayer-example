433f550697cbcb3fd2ec32d8f765ea3c039175a1
Sean Delaney <hello@delaneymethod.com>
2021-04-20 14:08:45 +0100

    Hooked up progress bars
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,30 @@
 			<section id="monster" class="container">
 				<h2>Monster Health</h2>
 				<div class="healthBar">
-					<div class="healthBar__value"></div>
+					<div :style="monsterBarStyles" class="healthBar__value" />
 				</div>
 			</section>
 			<section id="player" class="container">
 				<h2>Your Health</h2>
 				<div class="healthBar">
-					<div class="healthBar__value"></div>
+					<div :style="playerBarStyles" class="healthBar__value" />
 				</div>
 			</section>
+			<section v-if="winner" class="container">
+				<h2>Game Over!</h2>
+				<h3 v-if="winner === 'player'">You won!</h3>
+				<h3 v-if="winner === 'monster'">You lost!</h3>
+				<h3 v-if="winner === 'draw'">It's a draw!</h3>
+			</section>
 			<section id="controls">
-				<button>ATTACK</button>
-				<button>SPECIAL ATTACK</button>
-				<button>HEAL</button>
+				<button @click="attackMonster">ATTACK</button>
+				<button
+					:disabled="canUseSpecialAttackMonster"
+					@click="specialAttackMonster"
+				>
+					SPECIAL ATTACK
+				</button>
+				<button @click="healPlayer">HEAL</button>
 				<button>SURRENDER</button>
 			</section>
 			<section id="log" class="container">
@@ -39,12 +50,94 @@
 import HelloWorld from '@/components/HelloWorld.vue';
 */
 
+const getRandomValue = (min, max) =>
+	Math.floor(Math.random() * (max - min)) + min;
+
 export default {
 	name: 'App',
 	components: {
 		/*
 		HelloWorld
 		*/
+	},
+	data: () => ({
+		playerHealth: 100,
+		monsterHealth: 100,
+		currentRound: 0,
+		winner: null
+	}),
+	computed: {
+		canUseSpecialAttackMonster() {
+			return this.currentRound % 3 !== 0;
+		},
+		monsterBarStyles() {
+			return {
+				width: this.monsterHealth + '%'
+			};
+		},
+		playerBarStyles() {
+			return {
+				width: this.playerHealth + '%'
+			};
+		}
+	},
+	watch: {
+		monsterHealth(value) {
+			if (value <= 0 && this.playerHealth <= 0) {
+				// A draw
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				// Monster lost
+				this.winner = 'player';
+			}
+		},
+		playerHealth(value) {
+			if (value <= 0 && this.monsterHealth <= 0) {
+				// A draw
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				// Player lost
+				this.winner = 'monster';
+			}
+		}
+	},
+	methods: {
+		attackMonster() {
+			this.currentRound++;
+
+			const attackValue = getRandomValue(5, 12);
+
+			this.monsterHealth -= attackValue;
+
+			this.attackPlayer();
+		},
+		attackPlayer() {
+			const attackValue = getRandomValue(8, 15);
+
+			this.playerHealth -= attackValue;
+		},
+		specialAttackMonster() {
+			this.currentRound++;
+
+			const attackValue = getRandomValue(10, 25);
+
+			this.monsterHealth -= attackValue;
+
+			this.attackPlayer();
+		},
+		healPlayer() {
+			this.currentRound++;
+
+			const healValue = getRandomValue(8, 20);
+
+			if (this.playerHealth + healValue > 100) {
+				this.playerHealth = 100;
+			} else {
+				this.playerHealth += healValue;
+			}
+
+			this.attackPlayer();
+		}
 	}
 };
 </script>
